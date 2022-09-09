195157c308e690754277d20432b8c92d24d06eec
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2022-09-09 14:32:28 -0600

    Adjust messages and exceptions

--- source/main.py
+++ source/main.py
@@ -6,11 +6,11 @@ from tkinter import filedialog, messagebox, Tk, ttk
 
 # Define valid image file extensions.
 file_extensions = [
-    ("All Files", ".bmp .jpg .jpeg .png .tga"),
-    ("BMP Files", ".bmp"),
-    ("JPG Files", ".jpg .jpeg"),
-    ("PNG Files", ".png"),
-    ("TGA Files", ".tga"),
+    ["All Files", ".bmp .jpg .jpeg .png .tga"],
+    ["BMP Files", ".bmp"],
+    ["JPG Files", ".jpg .jpeg"],
+    ["PNG Files", ".png"],
+    ["TGA Files", ".tga"],
 ]
 
 # Check to make sure RPX is provided before building.
@@ -24,26 +24,22 @@ def check_package_status():
 def select_content_path():
     folder_directory = filedialog.askdirectory()
 
-    if not folder_directory:
-        raise Exception("No file was selected!")
-
-    content_path.config(state="normal")
-    content_path.delete(0, "end")
-    content_path.insert(0, folder_directory)
-    content_path.config(state="readonly")
+    if folder_directory:
+        content_path.config(state="normal")
+        content_path.delete(0, "end")
+        content_path.insert(0, folder_directory)
+        content_path.config(state="readonly")
 
 # Select the RPX file, the only necessary requirement.
 def select_rpx_file():
     file_directory = filedialog.askopenfilename(
         filetypes=[("RPX Files", ".rpx")])
 
-    if not file_directory:
-        raise Exception("No file was selected!")
-
-    rpx_path.config(state="normal")
-    rpx_path.delete(0, "end")
-    rpx_path.insert(0, file_directory)
-    rpx_path.config(state="readonly")
+    if file_directory:
+        rpx_path.config(state="normal")
+        rpx_path.delete(0, "end")
+        rpx_path.insert(0, file_directory)
+        rpx_path.config(state="readonly")
 
     check_package_status()
 
@@ -51,52 +47,49 @@ def select_rpx_file():
 def select_icon_file():
     file_directory = filedialog.askopenfilename(filetypes=file_extensions)
 
-    if not file_directory:
-        raise Exception("No file was selected!")
-    else:
+    if file_directory:
         application_image = Image.open(file_directory)
         if application_image.size != (128, 128):
             messagebox.showwarning("WUHB Packager",
-            "The picture you selected is not the right dimension!")
+                "The picture you selected is not the correct size, so it will "
+                + "be resized during packaging to fit correctly.")
 
-    icon_path.config(state="normal")
-    icon_path.delete(0, "end")
-    icon_path.insert(0, file_directory)
-    icon_path.config(state="readonly")
+        icon_path.config(state="normal")
+        icon_path.delete(0, "end")
+        icon_path.insert(0, file_directory)
+        icon_path.config(state="readonly")
 
 # Select the splash screen image for the television.
 def select_tv_splash_file():
     file_directory = filedialog.askopenfilename(filetypes=file_extensions)
 
-    if not file_directory:
-        raise Exception("No file was selected!")
-    else:
+    if file_directory:
         application_image = Image.open(file_directory)
         if application_image.size != (1280, 720):
             messagebox.showwarning("WUHB Packager",
-            "The picture you selected is not the right dimension!")
+                "The picture you selected is not the correct size, so it will "
+                + "be resized during packaging to fit correctly.")
 
-    tv_splash_path.config(state="normal")
-    tv_splash_path.delete(0, "end")
-    tv_splash_path.insert(0, file_directory)
-    tv_splash_path.config(state="readonly")
+        tv_splash_path.config(state="normal")
+        tv_splash_path.delete(0, "end")
+        tv_splash_path.insert(0, file_directory)
+        tv_splash_path.config(state="readonly")
 
 # Select the splash screen image for the Wii U GamePad.
 def select_drc_splash_file():
     file_directory = filedialog.askopenfilename(filetypes=file_extensions)
 
-    if not file_directory:
-        raise Exception("No file was selected!")
-    else:
+    if file_directory:
         application_image = Image.open(file_directory)
         if application_image.size != (854, 480):
             messagebox.showwarning("WUHB Packager",
-            "The picture you selected is not the right dimension!")
+                "The picture you selected is not the correct size, so it will "
+                + "be resized during packaging to fit correctly.")
 
-    drc_splash_path.config(state="normal")
-    drc_splash_path.delete(0, "end")
-    drc_splash_path.insert(0, file_directory)
-    drc_splash_path.config(state="readonly")
+        drc_splash_path.config(state="normal")
+        drc_splash_path.delete(0, "end")
+        drc_splash_path.insert(0, file_directory)
+        drc_splash_path.config(state="readonly")
 
 # Where all the core functions happen, packaging to a WUHB.
 def package_wuhb_file():
@@ -112,72 +105,70 @@ def package_wuhb_file():
     wuhb = filedialog.asksaveasfilename(
         filetypes=[("WUHB Files", ".wuhb")], defaultextension=".wuhb")
 
-    if not wuhb:
-        raise Exception("No file was selected!")
+    if wuhb:
+        if not name or not short_name or not author:
+            answer = messagebox.askyesno(title="WUHB Packager",
+                message="One or more fields of metadata are empty. Instead, any missing "
+                + "information will be filled with placeholder text. Is this okay?")
+            if not answer:
+                return
 
-    if not name or not short_name or not author:
-        answer = messagebox.askyesno(title="Missing Metadata",
-            message="One or more fields of metadata are empty. Instead, any missing "
-            + "information will be auto-filled with dummy text. Is this okay?")
+        if not icon or not tv_image or not drc_image:
+            answer = messagebox.askyesno(title="WUHB Packager",
+                message="One or more image files are missing. Instead, placeholder "
+                + "image files will be used during packaging. Is this okay?")
+            if not answer:
+                return
 
-        if not answer:
-            raise Exception("No metadata was given!")
+        if os.name == "nt":
+            # Confirmed that the user is running Windows.
+            command = "C:\\devkitPro\\tools\\bin\wuhbtool.exe"
+        elif os.name == "posix":
+            # Confirmed that the user is running Linux.
+            command = "/opt/devkitpro/tools/bin/wuhbtool"
+        else:
+            # The user is using an operating system that probably isn't supported.
+            messagebox.showerror("WUHB Packager", "Homebrew cannot be packaged on "
+            + "this system. Please try again on a Linux or Windows operating system.")
 
-    if not icon or not tv_image or not drc_image:
-        answer = messagebox.askyesno(title="Missing Image Files",
-            message="One or more image files are missing. Instead, dummy image "
-            + "files will be used during packaging. Is this okay?")
-            
-        if not answer:
-            raise Exception("No images were selected!")
+        if not os.path.exists(command):
+            # wuhbtool might not be installed or configured correctly.
+            return messagebox.showerror("WUHB Packager", "You must have wuhbtool "
+            + "installed properly in order to package an application. PLease check "
+            + " the WUHB Packager repository page for more information.")
+        
+        command += f" \"{rpx}\" \"{wuhb}\""
 
-    if os.name == "nt":
-        # Confirmed that the user is running Windows.
-        command = "C:\\devkitPro\\tools\\bin\wuhbtool.exe"
-    elif os.name == "posix":
-        # Confirmed that the user is running Linux.
-        command = "/opt/devkitpro/tools/bin/wuhbtool"
-    else:
-        # The user is using an operating system that probably isn't supported.
-        messagebox.showerror("WUHB Packager", "An application cannot be packaged on"
-        + "this system. Please run this on a Linux or Windows operating system.")
+        if content:
+            # Check for optional /content folder.
+            command += f' --content="{content}"'
+        if name:
+            # Check for optional application name.
+            command += f' --name "{name}"'
+        if short_name:
+            # Check for optional application short name.
+            command += f' --short-name "{short_name}"'
+        if author:
+            # Check for optional application author.
+            command += f' --author "{author}"'
+        if icon:
+            # Check for optional icon image.
+            command += f' --icon="{icon}"'
+        if tv_image:
+            # Check for optional television splash image.
+            command += f' --tv-image="{tv_image}"'
+        if drc_image:
+            # Check for optional Wii U GamePad splash image.
+            command += f' --drc-image="{drc_image}"'
 
-    if not os.path.exists(command):
-        # wuhbtool might not be installed or configured correctly.
-        messagebox.showerror("WUHB Packager", "You must have wuhbtool installed"
-        + " properly in order to package an application.")
-        raise Exception("wuhbtool is not installed or configured properly!")
-    
-    command += f" \"{rpx}\" \"{wuhb}\""
+        # Disable the package button, run the command, and inform when finished.
+        package_wuhb_button.config(text="Processing Package", state="disabled")
 
-    if content:
-        # Check for optional /content folder.
-        command += f' --content="{content}"'
-    if name:
-        # Check for optional application name.
-        command += f' --name "{name}"'
-    if short_name:
-        # Check for optional application short name.
-        command += f' --short-name "{short_name}"'
-    if author:
-        # Check for optional application author.
-        command += f' --author "{author}"'
-    if icon:
-        # Check for optional icon image.
-        command += f' --icon="{icon}"'
-    if tv_image:
-        # Check for optional television splash image.
-        command += f' --tv-image="{tv_image}"'
-    if drc_image:
-        # Check for optional Wii U GamePad splash image.
-        command += f' --drc-image="{drc_image}"'
-
-    # Disable the package button, run the command, and inform when finished.
-    package_wuhb_button.config(text=f"Processing Package", state="disabled")
-
-    os.system(command)
-    messagebox.showinfo("WUHB Packager", f"The application was packaged successfully!")
-    package_wuhb_button.config(text=f"Package WUHB File", state="normal")
+        os.system(command)
+        messagebox.showinfo("WUHB Packager", "The application was packaged successfully! "
+        + "If something seems incorrect, please try again or report the issue to the "
+        + "WUHB Packager repository page.")
+        package_wuhb_button.config(text="Package WUHB File", state="normal")
 
 # Create the Tkinter window and assets.
 main_window = Tk()
@@ -240,8 +231,7 @@ pick_tv_splash_file.config(height=1, width=35)
 # Wii U GamePad splash image selection panel.
 drc_splash_label = tkinter.Label(main_window, text="DRC Splash Screen Image (854x480)")
 drc_splash_label.grid(column=2, row=8)
-drc_splash_path = tkinter.Entry(
-    main_window, state="readonly", width=40, border=5)
+drc_splash_path = tkinter.Entry(main_window, state="readonly", width=40, border=5)
 drc_splash_path.grid(column=2, row=9)
 pick_drc_splash_file = tkinter.Button(main_window, text="Select DRC Splash Screen Image",
     border=5, command=select_drc_splash_file)
@@ -259,8 +249,7 @@ package_wuhb_button.config(height=2, width=35)
 button_separator = ttk.Separator(main_window, orient="horizontal")
 button_separator.grid(column=1, row=13, sticky="ew", pady=10)
 # Credits line to show who made the application.
-credit_label = tkinter.Label(
-    main_window, text="Created by NoahAbc12345", width=40)
+credit_label = tkinter.Label(main_window, text="Created by NoahAbc12345", width=40)
 credit_label.grid(column=1, row=14)
 
 # Make sure packaging button is disabled on start-up.
