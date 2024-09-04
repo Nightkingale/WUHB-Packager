9162e6731c6858690c8511581285b74bffef507e
NicoAICP <24915669+NicoAICP@users.noreply.github.com>
2024-09-04 06:11:03 +0200

    Filepicker error handling

--- source/main.py
+++ source/main.py
@@ -280,7 +280,7 @@ try:
         import winreg
         # Confirmed that the user is running Windows.
         registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-            r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\devkitProUpdater",
+            r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\devkitProUpdaters",
             access=winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
         install_location = winreg.QueryValueEx(registry_key, "InstallLocation")
         wuhbtool_file = install_location[0] + r"\tools\bin\wuhbtool.exe"
@@ -302,5 +302,13 @@ except (FileNotFoundError, KeyError) as error:
         wuhbtool_file = filedialog.askopenfilename(filetypes=file_ext)
     else:
         wuhbtool_file = filedialog.askopenfilename()
+
+    if wuhbtool_file == "":
+        messagebox.showerror("WUHB Packager","User cancled File Picking.\nProgram will now terminate.")
+        os._exit(0)
+    wuhbtool_fn = os.path.splitext(os.path.basename(wuhbtool_file))[0]
+    if "wuhbtool" not in wuhbtool_fn.lower():
+        messagebox.showerror("WUHB Packager","Could not verify that the provided tool is wuhbtool.\nProgram will now terminate.")
+        os._exit(0)
 
 main_window.mainloop()
